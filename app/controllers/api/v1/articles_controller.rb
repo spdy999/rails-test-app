1acde0c9cf56aa7eb3ae7bf5656c5734da9a7624
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-12-02 03:55:39 +0700

    add create method to article controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,21 @@ module Api
         article = Article.find(params[:id])
         render json: {status: 'SUCCESS', message: 'Loaded article', data:article}, status: :ok
       end
+
+      def create # TODO: fix  :InvalidAuthenticityToken (security things ¯\_(ツ)_/¯)
+        article = Article.create(article_params)
+
+        if article.save
+          render json: {status: 'SUCCESS', message: 'Saved article', data:article}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Article not saved', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def article_params
+        params.permit(:title, :body)
+      end
     end
   end
 end
