8ddeb1f9f452f290cef7c1a72d30be7db9db20a8
avenger999 <1a1a1a.262626.0010@gmail.com>
2019-12-05 20:06:23 +0700

    update articles_controller create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,10 @@ class ArticlesController < ApplicationController
   def create
     render plain: params[:article].inspect
     # @article = Article.new(article_params)
-    # @article.save
-    # redirect_to article_path(@article)
+    # if @article.save
+    #   flash[:success] = "Article was successfully created"
+    #   redirect_to article_path(@article)
+    # end
   end
 
   private
