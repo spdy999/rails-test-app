b951ed59c0cf4314a77d707ad0f8bc0251cca672
avenger999 <1a1a1a.262626.0010@gmail.com>
2019-12-06 01:53:19 +0700

    add get users and create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,26 @@
 class ArticlesController < ApplicationController
   def index # http://localhost:3000/articles
     articles = Article.order('created_at DESC')
-    render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
+    render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
   end
+
   def new
     @article = Article.new
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def create
-    render plain: params[:article].inspect
+    article = Article.create(article_params)
+
+    if article.save
+      render json: {status: 'SUCCESS', message: 'Saved article', data: article}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, status: :unprocessable_entity
+    end
+    #render plain: params[:article].inspect
     # @article = Article.new(article_params)
     # if @article.save
     #   flash[:success] = "Article was successfully created"
@@ -19,7 +29,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
